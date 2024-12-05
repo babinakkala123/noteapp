8b6c083961457e0c090ea82a41ae523d75da7b0e
babinakkala123 <nakkalababi123@gmail.com>
2024-12-05 10:10:09 +0530

    added app.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import api from './api';
+import Navbar from './components/Navbar';
+import NotesList from './components/NotesList';
+import NoteForm from './components/NoteForm';
 import './App.css';
+ 
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [selectedNote, setSelectedNote] = useState(null);
 
-function App() {
+  const fetchNotes = async () => {
+    const response = await api.get('/');
+    setNotes(response.data);
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const handleSave = async (note) => {
+    if (note.id) {
+      await api.put(`/${note.id}`, { title: note.title, content: note.content });
+    } else {
+      await api.post('/', { title: note.title, content: note.content });
+    }
+    fetchNotes();
+    setSelectedNote(null);
+  };
+
+  const handleDelete = async (id) => {
+    await api.delete(`/${id}`);
+    fetchNotes();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <Navbar />
+       <div className="content">
+       <NoteForm selectedNote={selectedNote} onSave={handleSave} />
+       <NotesList notes={notes} onEdit={setSelectedNote} onDelete={handleDelete} />
+       </div>
     </div>
-  );
-}
-
-export default App;
+    );
+  };
+  export default App;  
